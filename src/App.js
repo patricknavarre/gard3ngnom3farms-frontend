578a6b430b4f1ce51eae735079bc5f5f57062842
Patrick <patnavarre@gmail.com>
2021-08-21 15:49:03 -0400

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import './App.css';
+
+import Navbar from "./components/Navbar/Navbar"
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      
+      <Navbar />
     </div>
+
+    </Router>
   );
 }
 
