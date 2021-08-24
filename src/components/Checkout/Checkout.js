8b498703ee441124090567457666d0257f5f57a2
Patrick <patnavarre@gmail.com>
2021-08-23 21:02:50 -0400

    datePicker added

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,17 +1,27 @@
 import styles from './Checkout.module.css';
-import React from 'react'
+import React , {useState} from 'react'
+import DatePicker from 'react-date-picker';
 
 import { connect } from "react-redux"
 
 const Checkout = ({ cart }) => {
+
+  const [value, onChange] = useState(new Date())
   return (
     <div className={styles.checkout}>
      <form>
      <div>
+    <h2>Please Enter Your Email & Date You'd Like to Pickup your Order, Thank You!</h2>
   <label htmlFor="inputBuyerEmail" className={styles.checkout_input}>
     Email:
     <input type="text" name="buyerEmail" />
   </label>
+     </div>
+     <div>
+  <DatePicker 
+   onChange={onChange}
+   value={value}
+   className={styles.datepicker}/>
      </div>
   <input type="submit" value="Submit" />
 </form>
